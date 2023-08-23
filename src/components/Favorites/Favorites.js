44d8bf409b595397e673a34352bc4399cd57f70a
Bryce Schultz <bryceschultz26@gmail.com>
2023-08-23 11:55:11 -0400

    adding getfavorites and making favorites a context

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,29 @@
+import Table from 'react-bootstrap/Table';
+import { useFavorites } from '../../hooks/useFavorites';
+
+const Favorites = () => {
+    const { favorites } = useFavorites();
+
+    return (
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>State</th>
+            <th>Favorite Name</th>
+          </tr>
+        </thead>
+        <tbody>
+            {favorites && favorites.map(({city, state, favoriteLocation}) => (
+                <tr>
+                    <td>{city}</td>
+                    <td>{state}</td>
+                    <td>{favoriteLocation}</td>
+                </tr>
+            ))}
+        </tbody>
+      </Table>
+    )
+}
+
+export default Favorites
