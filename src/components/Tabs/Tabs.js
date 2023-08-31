5dbb489716c0c22d8550aa56fc1ec566f0f59971
Bryce Schultz <bryceschultz26@gmail.com>
2023-08-31 15:05:48 -0400

    removing favorites tab if there are no favorites

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import { useFavorites } from "../../hooks/useFavorites";
 import AqiInformation from "../AqiInformation/AqiInformation";
 import Favorites from "../Favorites/Favorites";
 import "./Tabs.css";
 
 const AQITabs = () => {
+  const { favorites } = useFavorites();
+
   return (
     <Tabs
-      defaultActiveKey="Favorites"
       id="uncontrolled-tab-example"
       className="mb-3 mt-5"
     >
-      <Tab eventKey="Favorites" title="Favorites" className="favorites-tabitem">
-        <Favorites />
-      </Tab>
+      {favorites.length > 0 &&
+        <Tab 
+          eventKey="Favorites" 
+          title="Favorites" 
+          className="favorites-tabitem"
+          >
+          <Favorites />
+        </Tab>
+      }
       <Tab
         eventKey="AQIInformation"
         title="AQI Information"
